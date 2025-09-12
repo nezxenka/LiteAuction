f8a14aa70624490ee11a258abe8a7cc0ac39d9ab
nezxenka <kuznetsovaleks2009@mail.ru>
2025-09-12 14:30:20 +0300

    Delete src/main directory

--- src/main/java/ru/nezxenka/liteauction/backend/exceptions/UnsupportedConfigurationException.java
+++
@@ -1,7 +0,0 @@
-package ru.nezxenka.liteauction.backend.exceptions;
-
-public class UnsupportedConfigurationException extends RuntimeException {
-    public UnsupportedConfigurationException(String message) {
-        super(message);
-    }
-}
