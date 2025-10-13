72f97ab41ace3fa010bc50a5f6739910ad27d976
nezxenka <kuznetsovaleks2009@mail.ru>
2025-10-13 05:26:30 +0300

    Delete src directory

--- src/main/java/ru/nezxenka/liteauction/backend/enums/AuctionType.java
+++
@@ -1,5 +0,0 @@
-package ru.nezxenka.liteauction.backend.enums;
-
-public enum AuctionType {
-    MARKET, BIDS
-}
--- src/main/java/ru/nezxenka/liteauction/economy/EconomyEditor.java
+++
@@ -1,7 +0,0 @@
-package ru.nezxenka.liteauction.economy;
-
-public abstract class EconomyEditor {
-    public abstract double getBalance(String player);
-    public abstract void addBalance(String player, double count);
-    public abstract void subtractBalance(String player, double count);
-}
